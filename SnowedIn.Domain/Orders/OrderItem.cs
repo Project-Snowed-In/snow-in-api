0ede7c4b7154b2a1c9519a1c2eea0f1faa8f5c41
Tyler Noble <tnobs3339@gmail.com>
2022-04-17 16:20:42 -0400

    Review

---
+++ SnowedIn.Domain/Orders/OrderItem.cs
@@ -0,0 +1,12 @@
+using SnowedIn.Domain.Catalog;
+
+namespace SnowedIn.Domain.Orders
+{
+    public class OrderItem
+    {
+        public int Id { get; set; }
+        public Item Item { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price => Item.Price * Quantity;
+    }
+}
