be2db3ecc8ccfbf378e14d4275eb61253e0ae889
kmmorishita <morishita.5@osu.edu>
2022-04-21 16:10:46 -0400

    Rating tests complete

---
+++ tests/SnowedIn.Domain.Tests/RatingTests.cs
@@ -0,0 +1,33 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SnowedIn.Domain.Catalog;
+using System;
+
+namespace SnowedIn.Domain.Tests
+{
+
+    [TestClass]
+    public class RatingTests
+    {
+        [TestMethod]
+        public void Can_Create_New_Rating()
+        {
+            //Arrange
+            var rating = new Rating(1, "Mike", "Great fit!");
+
+            //Act (Empty)
+
+            //Assert
+        Assert.AreEqual(1, rating.Stars);
+        Assert.AreEqual("Mike", rating.UserName);
+        Assert.AreEqual("Great fit!", rating.Review);
+
+        }
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Cannot_Create_Rating_With_Invalid_Stars()
+        {
+            // Arrange
+            var rating = new Rating(0, "Mike", "Great fit!");
+        }
+    }
+}
